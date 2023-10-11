827e4a4053e768d46970923a2a78a691d037afa7
A-Zap <0011azap@gmail.com>
2023-10-11 16:06:41 -0700

    Create Booleans.py
    test test test

---
+++ Day 4/booleans.py
@@ -0,0 +1,19 @@
+var1= True
+var2= False
+print(type(var1))
+print(var1)
+
+number= 5>=2+3
+print(type(number))
+print(number)
+
+
+numB= bool(5<6)
+print(type(numB))
+print(numB)
+
+
+list= [1,2,3,4]
+control= 5 in list
+print(type(control))
+print(control)
